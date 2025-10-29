446c959f53adc6a1182ce4e07e268bba10c7b0d3
milanaarus <milanaarusanova@gmail.com>
2025-10-29 10:53:35 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,22 @@
-
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+string = input('Введите строку, состоящую из латинских букв: ')
+def clear(string):
+    dictionary = {}
+    for letter in string:
+        if letter.isalpha():
+            lower_letter = letter.lower()
+            if lower_letter not in dictionary:
+                dictionary[lower_letter] = {'lower': 0, 'upper': 0}
+            if lower_letter == letter:
+                dictionary[lower_letter]['lower'] += 1
+            else:
+                dictionary[lower_letter]['upper'] += 1
+    result = []
+    for letter in string:
+        if not letter.isalpha():
+            result.append(letter)
+            continue
+        lower_letter = letter.lower()
+        if dictionary[lower_letter]['lower'] <= dictionary[lower_letter]['upper']:
+            result.append(letter)
+    return ''.join(result)
+print(clear(string))
