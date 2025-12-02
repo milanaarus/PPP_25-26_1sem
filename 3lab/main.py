49c87ca6976ba4971c9411b0712327931a4ed959
milanaarus <milanaarusanova@gmail.com>
2025-12-02 13:02:15 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,25 @@
+import os
+
+def travell_directory(path):
+    way = {}
+    try:
+        items = os.listdir(path)
+    except PermissionError:
+        print(f'Нет доступа к директории: {path}')
+        return way
+
+    for item in items:
+        full_way = os.path.join(path, item)
+        
+        if os.path.isdir(full_way):
+            way[item] = travell_directory(full_way)
+        else:
+            way[item] = None  
+    
+    return way
 
 
 if __name__ == "__main__":
-    pass # Ваш код здесь
+    directory_path = 'путь/к/директории'
+    result = travell_directory(directory_path)
+    print(result)
